f818eb18b603ed6f7cbe16347302736bdaa905e1
bodasooqa <timlol9894@gmail.com>
2025-10-03 18:44:07 +0700

    add 11-container-with-most-water problem

---
+++ leetcode/11-container-with-most-water.ts
@@ -0,0 +1,20 @@
+function maxArea(height: number[]): number {
+    let left = 0, right = height.length - 1, ans = 0;
+
+    while (left < right) {
+        // Считаем площадь, обновляем максимум
+        const area = Math.min(height[left], height[right]) * (right - left);
+        ans = Math.max(ans, area);
+
+        // Двигаем указатель, который указывает на меньшую высоту
+        if (height[left] < height[right]) left++;
+        else right--;
+    }
+
+    return ans;
+}
+
+console.log(maxArea([1,8,6,2,5,4,8,3,7])); // 49
+console.log(maxArea([1,1])); // 1
+console.log(maxArea([4,3,2,1,4])); // 16
+console.log(maxArea([1,2,1])); // 2
